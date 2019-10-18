c59ce21641ee821891a72a599f144d29f9ea75a1
Gabriel Alegrete <gabriel.alegrete@doc88.com.br>
2019-10-18 17:21:35 -0300

    config pastel-de-ideias-doc

--- pastel-de-ideias-doc/src/main.js
+++ pastel-de-ideias-doc/src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm'
 import App from './App.vue'
+import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueMaterial)
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  data () {
+    return {
+      value: true
+    }
+  }
+})
