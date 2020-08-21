4e192393a5c3cd627ad90ed6c0b4e38e2c09edb4
gabrielalegrete <gabrielalegrete1@gmail.com>
2020-08-20 21:01:24 -0300

    Landpage_0.0.2

---
+++ gabriel.alegrete/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Land />
+  </div>
+</template>
+
+<script>
+import Land from './components/Land.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Land
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url('../../src/assets/images/FB_IMG_1547514970654 (1).jpg');
+  background-size: cover;
+  height: 100%;
+  background-attachment: scroll;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+</style>
---
+++ gabriel.alegrete/src/components/Land.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="screen">
+        name:
+        <input type="text" name="name">
+
+        e-mail:
+        <input type="text" name="email">
+
+        phone number:
+        <input type="number" name="phone">
+
+        <input type="submit" name="save_login" value="send">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Land'
+}
+</script>
+
+<style scoped>
+    .screen{
+        justify-items: center;
+        display: grid;
+    }
+    input {
+        margin: 5px;
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
