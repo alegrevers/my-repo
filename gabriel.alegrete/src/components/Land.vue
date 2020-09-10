d79dbd8b555fbe709e250c301d7e2cad42a76f0b
gabrielalegrete <gabrielalegrete1@gmail.com>
2020-09-10 02:46:03 -0300

    Landpage_0.0.8 main

--- gabriel.alegrete/src/components/Land.vue
+++ gabriel.alegrete/src/components/Land.vue
@@ -12,20 +12,67 @@
                     Tenha as <em>melhores</em> e mais <em>práticas</em> aulas de <em>guitarra</em> e <em>violão</em> comigo!
                 </h1>
                 <div class="main__content__inputs">
-                    <input class="main__content__inputs--forms" placeholder="o nome que sua mãe te deu (:" type="text" name="name">
+                    <input class="main__content__inputs--forms" placeholder="o nome que sua mãe te deu (:" type="text" name="name" required>
 
-                    <input class="main__content__inputs--forms" placeholder="o e-mail que tu mais usa" type="text" name="email">
+                    <input class="main__content__inputs--forms" placeholder="o e-mail que tu mais usa" type="text" name="email" required>
 
-                    <input class="main__content__inputs--forms" placeholder="seu telefone (não é para passar trote)" type="number" name="phone">
+                    <input class="main__content__inputs--forms" placeholder="seu telefone (não é para passar trote)" type="number" name="phone" required>
 
                     <input class="main__content__inputs--register" type="submit" name="save_login" value="Cadastrar!">
                 </div>
             </div>
 
             <div class="main__content__image">
-                <img src="../../src/assets/images/guita_4.png">
+                <img class="main__content__image--cover" src="../../src/assets/images/guita_4.png">
                 encerramento da escola Espaço Garage - 2018
             </div>
+        </div>
+        <div class="separator"></div>
+        <div class="complement">
+            <div class="complement__text">
+                <div class="complement__text__explains">
+                    <h3>
+                        Participando das aulas, aprenda muitos macetes que vão te ajudar a se tornar 
+                        um músico com bastante <em>conhecimento técnico e feelings emotivos para 
+                        impressionar quem te ouve.</em>
+                    </h3>
+                    Durante as aulas, vocẽ poderá descobrir através de lições práticas, como a música funciona 
+                    por trás de toda a teoria, juntamente com dicas de como aplicar isso no dia à dia 
+                    dos seus estudos. Estudar música exige paciência para trabalhar os exercícios 
+                    repetitívos e persistência para memorizar todo o conteúdo que aprederá. Também trabalharemos 
+                    o desenvolvimento da identidade musical para auxiliar em composições e na hora de improvisar.
+                </div>
+                <div class="complement__text__bio">
+                    <h4>
+                        <span>Gabriel Alegrete, apelidado de "Jukebox"</span> teve o apelido confirmado por tocar estilos 
+                        variados em diversos eventos de estilos musicais distintos. Essa característica se faz presente 
+                        pois nos mais de 8 anos como músico sempre buscou aprender estilos diferentes mesmo tendo como 
+                        estilos principáis o Rock e o R&B. Hoje toca projetos autorais com banda e também busca conhecimento 
+                        na parte de Produção Musical.
+                    </h4>
+
+                    <div class="complement__text__bio--social-media">
+                        <a href="https://www.facebook.com/alegreteg" target="_blank">
+                            <img class="complement__text__bio--social-media--icons" src="../../src/assets/images/face-logo.png">
+                        </a>
+                        <a href="https://www.instagram.com/alegrete_/" target="_blank">
+                            <img class="complement__text__bio--social-media--icons" src="../../src/assets/images/insta-logo.png">
+                        </a>
+                        <a href="https://wa.me/+5511962730230" target="_blank">
+                            <img class="complement__text__bio--social-media--icons" src="../../src/assets/images/whats-logo.png">
+                        </a>
+                        <a href="https://mobile.twitter.com/alegrete__" target="_blank">
+                            <img class="complement__text__bio--social-media--icons" src="../../src/assets/images/twitter-logo.png">
+                        </a>
+                        <a style="display: none;" href="https://www.youtube.com/channel/UC6ir6G9nez7Fhdza4KBMsGw/featured" target="_blank">
+                            <img class="complement__text__bio--social-media--icons" src="../../src/assets/images/youtube-logo.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            Copyright © 2020 Gabriel Alegrete. Todos os direitos reservados
         </div>
     </div>
 </template>
@@ -42,6 +89,8 @@ export default {
         font-family: 'Roboto';
         font-size: 20px;
         height: 100%;
+        justify-items: center;
+        display: grid;
 
         &__content {
             display: flex;
@@ -85,24 +134,65 @@ export default {
                 font-size: 13px;
                 display: grid;
                 font-style: italic;
+
+                &--cover {
+                    margin-left: 100px;
+                }
             }
         }
     }
 
-    a {
-        background-color: transparent;
-        margin-right: 50px;
+    .complement {
+        padding-bottom: 50px;
+
+        &__text {
+            display: flex;
+            justify-content: center;
+            text-align: left;
+
+            &__explains {
+                width: 500px;
+                padding: 50px;
+                border-right: #ecad00 1px solid;
+            }
+
+            &__bio {
+                width: 500px;
+                display: grid;
+                padding: 50px;
+                border-left: #f58638 1px solid;
+
+                &--social-media {
+                    display: flex;
+                    justify-content: space-between;
+
+                    &--icons {
+                        height: 40px;
+                        cursor: pointer;
+                    }
+                }
+            }
+        }
+    }
+
+    .footer {
+        height: 100px;
+        background: linear-gradient(to right, #f58638, #feca3d);
         color: #2c3e50;
-        text-decoration: none;
-        border: none;
-        font-weight: bold;
-        cursor: pointer;
+        width: 100%;
+        font-size: 15px;
+    }
+    
+    .separator {
+        height: 2px;
+        width: 85%;
+        background: linear-gradient(to right, #f58638, #feca3d);
+        margin: 10px;
+        justify-content: center;
     }
 
     h1 {
-        // word-wrap: break-word;
         text-align: left;
-        color: white;
     }
 
     em {
@@ -110,6 +200,13 @@ export default {
         background: -webkit-linear-gradient(#feca3d, #f58638);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
+    }
+
+    span {
+        background: -webkit-linear-gradient(#feca3d, #f58638);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        margin: 0;
     }
 
     head {
@@ -122,10 +219,6 @@ export default {
         height: 35px;
     }
 
-    img {
-        margin-left: 100px;
-    }
-    
     input {
         outline: none;
         margin: 5px;
@@ -147,6 +240,10 @@ export default {
     input::-webkit-inner-spin-button {
         -webkit-appearance: none;
         margin: 0;
+    }
+
+    @media only screen and (max-device-width: 900px) {
+        /* .menu { width:100%; } */
     }
 
 </style>
