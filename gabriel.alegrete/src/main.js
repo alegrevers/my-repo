915feeabfd7b7bf7220a545d980985bc2d171b24
gabrielalegrete <gabrielalegrete1@gmail.com>
2020-09-10 02:49:38 -0300

    Landpage_0.0.9 main

--- gabriel.alegrete/src/main.js
+++ gabriel.alegrete/src/main.js
@@ -1,58 +1,58 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router';
+// import VueRouter from 'vue-router';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-const routes = [
-  {
-    path: '/',
-    component: App,
-    meta: {
-      title: 'Home Page - Example App',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The home page of our example app.'
-        },
-        {
-          property: 'og:description',
-          content: 'The home page of our example app.'
-        }
-      ]
-    }
-  },
-  {
-    path: '/about',
-    // I'm kind of cheating by reusing the main app component here.
-    component: App,
-    meta: {
-      title: 'About Page - Example App',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The about page of our example app.'
-        },
-        {
-          property: 'og:description',
-          content: 'The about page of our example app.'
-        }
-      ]
-    },
+// const routes = [
+//   {
+//     path: '/',
+//     component: App,
+//     meta: {
+//       title: 'Home Page - Example App',
+//       metaTags: [
+//         {
+//           name: 'description',
+//           content: 'The home page of our example app.'
+//         },
+//         {
+//           property: 'og:description',
+//           content: 'The home page of our example app.'
+//         }
+//       ]
+//     }
+//   },
+//   {
+//     path: '/about',
+//     // I'm kind of cheating by reusing the main app component here.
+//     component: App,
+//     meta: {
+//       title: 'About Page - Example App',
+//       metaTags: [
+//         {
+//           name: 'description',
+//           content: 'The about page of our example app.'
+//         },
+//         {
+//           property: 'og:description',
+//           content: 'The about page of our example app.'
+//         }
+//       ]
+//     },
 
-    children: [
-      {
-      }
-    ]
-  }
-];
+//     children: [
+//       {
+//       }
+//     ]
+//   }
+// ];
 
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-});
+// const router = new VueRouter({
+//   routes,
+//   mode: 'history'
+// });
 
 new Vue({
-  router,
+  // router,
   render: h => h(App),
 }).$mount('#app')
